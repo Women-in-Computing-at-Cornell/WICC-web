3916f1e51a38ec2908add8fa928f57551e7dde8e
Anna Nguyen <axn3@cornell.edu>
2021-02-04 19:51:36 -0500

    Set up join us page. Also implemented some styling to Home and Join Us pages.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image'
+
+import './Home.css';
+import illustration from "../images/homepage-illustration.png";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Women in Computing at Cornell</h1>
-      <h3>strives to make computing inclusive for all.</h3>
-      <h4>Our Mission</h4>
-      <p>We aim to foster a supportive community of women and allies* equipped with the resources needed to recognize and overcome challenges. By creating opportunities for technical and leadership growth, we work to ensure that people of all identities are able to discover and pursue their interests and talents in order to positively impact the future of tech.</p>
-      <p>* all gender identities are welcomed and supported!</p>
-      <h4>Believe in this mission?</h4>
-      <Button variant="light" href="/join">Get Involved</Button>
+    <div class="home-container">
+      <section class="hero-container">
+        <Image src={illustration} id="home-illustration" />
+        <div style={{ margin: "5% 0" }}>
+          <h1 style={{ marginBottom: "2.5%" }}>Women in Computing at Cornell</h1>
+          <h2>strives to make computing inclusive for all.</h2>
+        </div>
+      </section>
+      <section class="mission-container">
+        <h2>Our Mission</h2>
+        <div>
+          <p>We aim to foster a <strong>supportive community</strong> of women and
+        allies* equipped with the resources needed to recognize
+        and overcome challenges. By creating <strong>opportunities for
+        technical and leadership growth</strong>, we work to ensure that
+        people of all identities are able to discover and pursue
+        their interests and talents in order to <strong>positively impact
+        the future of tech.</strong></p>
+          <p>* all gender identities are welcomed and supported!</p>
+        </div>
+      </section>
+      <section class="call-to-action-container">
+        <h4>Believe in this mission?</h4>
+        <Button href="/join">Get Involved</Button>
+      </section>
     </div>
   );
 }
