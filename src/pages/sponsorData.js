69eb074f31a0814e1dd619f85ab4e3021a1f2043
Aroob Jalil <asj49@cornell.edu>
2022-10-15 15:10:51 -0400

    Co-authored-by: rahma-a04 <rahma-a04@users.noreply.github.com> Co-authored-by: ky322 <ky322@users.noreply.github.com>

--- src/pages/sponsorData.js
+++ src/pages/sponsorData.js
@@ -1,17 +1,78 @@
 export const sponsorData = [
+
+  //Platinum Sponsors
+
   {
-    "sponsor": "Bloomberg",
-    "level": "Gold",
-    "link": "https://www.bloomberg.com/company/"
+    "sponsor": "Cruise",
+    "level": "Platinum",
+    "link": "https://getcruise.com/"
+  }, 
+  {
+    "sponsor": "Deloitte",
+    "level": "Platinum",
+    "link": "https://www2.deloitte.com/us/en.html"
+  },
+  {
+    "sponsor": "HRT",
+    "level": "Platinum",
+    "link": "https://www.hudsonrivertrading.com/"
   },
   {
     "sponsor": "Jane Street",
     "level": "Platinum",
     "link": "https://www.janestreet.com/"
   },
+
+  // Gold Sponsors
+
+  {
+    "sponsor": "Accenture",
+    "level": "Gold",
+    "link": "https://www.accenture.com/us-en"
+  },
+    {
+    "sponsor": "Bloomberg",
+    "level": "Gold",
+    "link": "https://www.bloomberg.com/company/"
+  },
+  {
+    "sponsor": "Capital One",
+    "level": "Gold",
+    "link": "https://www.capitalone.com/"
+  },
+  {
+    "sponsor": "DE Shaw",
+    "level": "Gold",
+    "link": "https://www.deshaw.com/"
+  },
+  {
+    "sponsor": "Duolingo",
+    "level": "Gold",
+    "link": "https://www.duolingo.com/"
+  },
+  {
+    "sponsor": "Goldman Sachs",
+    "level": "Gold",
+    "link": "https://www.goldmansachs.com/"
+  },
+  {
+    "sponsor": "MathWorks",
+    "level": "Gold",
+    "link": "https://www.mathworks.com/"
+  },
+  {
+    "sponsor": "Millennium Management",
+    "level": "Gold",
+    "link": "https://www.mlp.com/"
+  },
+  {
+    "sponsor": "Sandia National Labs",
+    "level": "Gold",
+    "link": "https://www.sandia.gov/"
+  },
   {
     "sponsor": "Stripe",
-    "level": "Silver",
+    "level": "Gold",
     "link": "https://stripe.com/"
   },
   {
@@ -20,43 +81,26 @@ export const sponsorData = [
     "link": "https://www.uber.com/us/en/careers/"
   },
   {
-    "sponsor": "Accenture",
-    "level": "Platinum",
-    "link": "https://www.accenture.com/us-en"
-  },
-  {
-    "sponsor": "Cruise",
-    "level": "Platinum",
-    "link": 'https://getcruise.com/'
-  },
-  {
-    "sponsor": "MathWorks",
-    "level": "Gold",
-    "link": "https://www.mathworks.com/"
-  },
-  {
-    "sponsor": "Zoom",
-    "level": "Gold",
-    "link": "https://zoom.us/"
-  },
-  {
-    "sponsor": "Capital One",
-    "level": "Gold",
-    "link": "https://www.capitalone.com/"
-  },
-  {
-    "sponsor": "Millennium Management",
-    "level": "Platinum",
-    "link": "https://www.mlp.com/"
-  },
-  {
-    "sponsor": "SambaNova",
-    "level": "Gold",
-    "link": "https://sambanova.ai/"
-},
-  {
     "sponsor": "Yext",
     "level": "Grace Hopper",
     "link": "https://www.yext.com/?keyword=yext&utm_source=google&utm_medium=paid_search&utm_campaign=PM_GSearch_ACQ_Branded_04.2022&utm_adgroup=&gclid=Cj0KCQjwpImTBhCmARIsAKr58cw43EPGQJEhvvtMIEiu2VmH9vi1j8QsG9TGmvpaR1WS3gCoRxzWmT4aAsY6EALw_wcB"
-  }
+  },
+
+  // silver sponsors
+  
+  {
+    "sponsor": "IMC Trading",
+    "level": "Silver",
+    "link": "https://www.imc.com/us/"
+  },
+  {
+    "sponsor": "Intel",
+    "level": "Silver",
+    "link": "https://www.intel.com/content/www/us/en/homepage.html"
+  },
+  {
+    "sponsor": "Meta",
+    "level": "Silver",
+    "link": "https://about.meta.com/"
+  },
 ]
