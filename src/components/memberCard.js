f236a6155c85593ec014f50cc19ffc6e8cb3083a
dorawengg <cw749@cornell.edu>
2023-11-12 14:56:39 -0500

    changed the styles for member component

--- src/components/memberCard.js
+++ src/components/memberCard.js
@@ -8,13 +8,24 @@ function Member({ name, title, linkedin, netid, bio, img }) {
       >
         <div>
           {/* Use the img prop passed to the component */}
-          <img style={{borderRadius: "10%", height:"200px", width:"100%", objectFit:"cover", float:"left"}} class="headshots" src={img} alt={name} />
+          <img
+            style={{
+              borderRadius: "10%",
+              height: "200px",
+              width: "100%",
+              objectFit: "cover",
+              float: "left",
+            }}
+            class="headshots"
+            src={img}
+            alt={name}
+          />
         </div>
         <div>
           <div>{name}</div>
         </div>
         <div>{title}</div>
-        <div>{bio}</div>
+        {/* <div>{bio}</div> */}
       </div>
     </>
   );
