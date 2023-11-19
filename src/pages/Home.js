6038609994a11c47ed78af52590df182d34c3c35
Alina <ay332@cornell.edu>
2023-11-19 15:59:11 -0500

    Restore navbar and homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image'
-
 import './Home.css';
 import illustration from "../images/homepage-illustration.png";
 import Sponsors from '../pages/Sponsors';
+import missionImg from "../images/colorgrouppic.png"
+import boardGroupPic from "../images/peoplegroupic.jpg"
+import PhotoCarousel from '../pages/PhotoCarousel';
+import mentorshipPic from '../images/programs-pictures/mentorship2.jpg'
+import outreachPic from '../images/outreach/outreach2.jpg'
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 
 const Home = () => {
   return (
     <div >
+      <Image style = {{maxWidth: '100%', height: 'auto'}} src={boardGroupPic} id="hero-image" />
       <div class="home-container" >
         <section class="hero-container" style={{
           backgroundImage: { illustration }
         }}>
-          <Image src={illustration} id="home-illustration" />
-          <div style={{ margin: "3% 0" }}>
+          <div>
             <h1
               style={{ marginBottom: "2%", fontWeight: "bold" }}
               class="headerName"
@@ -25,22 +31,34 @@ const Home = () => {
             <h2>strives to make computing inclusive for all</h2>
           </div>
         </section>
-        <section class="mission-container">
-          <h2 style={{ fontWeight: 'bold' }}>  Our Mission</h2>
-          <div>
-            <p style={{ paddingLeft: '10%', paddingRight: '10%' }}>We aim to foster a <strong>supportive community</strong> of women and
+        <section style = {{justifyContent: 'space-around'}}class="mission-container">
+        <div>
+            <Image style={{ maxWidth: '450px' }} src={missionImg} id="mission-image"></Image>
+          </div>
+            <div>
+            <h2 style={{ textAlign: 'left', fontWeight: 'bold' }}>  Our Mission</h2>
+            <p style={{textAlign: 'left', maxWidth: '570px'}}>We aim to foster a <strong>supportive community</strong> of women and
               allies* equipped with the resources needed to recognize
               and overcome challenges. By creating <strong>opportunities for
                 technical and leadership growth</strong>, we work to ensure that
               people of all identities are able to discover and pursue
               their interests and talents in order to <strong>positively impact
                 the future of tech.</strong></p>
-            <p>* All gender identities are welcomed and supported!</p>
+            <p style={{ textAlign: 'left'}}>* All gender identities are welcomed and supported!</p>
+            <h2 style={{ textAlign: 'left', fontWeight: 'bold' }}>  Semester Outlook</h2>
+            <p style={{textAlign: 'left', maxWidth: '570px'}}>WICC is a place where women 
+            and gender minorities feel supported throughout their academic journey and 
+            careers in technology. We are a coalition of leaders who seek to make an 
+            impact on Cornell, Ithaca and our greater community. Achieving this 
+            impact does not just involve WICC e-board, but also you (g-body members),
+             CIS staff, Cornell, and the greater Ithaca community!</p>
           </div>
-          <br /><br />
-          <h2 style={{ fontWeight: 'bold' }}>Our Sponsors</h2>
         </section>
-        <section class='sponsor-container'>
+        <section style={{paddingTop: '10px'}} class="carousel-container">
+        <PhotoCarousel imgNames={["img1","img2"]} pics={[mentorshipPic, outreachPic]} />
+      </section>
+        <section style = {{marginTop: '5%'}} class='sponsor-container'>
+        <h2 style={{ fontWeight: 'bold' }}>Our Sponsors</h2>
           <Sponsors />
         </section>
         <section class="call-to-action-container">
@@ -51,5 +69,4 @@ const Home = () => {
     </div>
   );
 }
-
 export default Home;
