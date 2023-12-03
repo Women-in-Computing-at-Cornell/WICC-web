1933a1fae775781e5d85c4f15db2cf73f332b1d7
Alina <ay332@cornell.edu>
2023-12-03 16:35:03 -0500

    Revamp top half of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,9 @@ import PhotoCarousel from '../pages/PhotoCarousel';
 import mentorshipPic from '../images/programs-pictures/mentorship2.jpg'
 import outreachPic from '../images/outreach/outreach2.jpg'
 import { Carousel } from 'react-responsive-carousel';
+import semoutlook from '../images/semesteroutlookpic.png';
+import newmissionImg from '../images/wiccnewlogo.png';
+
 
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
@@ -55,46 +58,41 @@ const Home = () => {
   
   return (
     <div >
-      <Image style = {{maxWidth: '100%', height: 'auto'}} src={boardGroupPic} id="hero-image" />
+      {/* <Image style = {{maxWidth: '100%', height: 'auto'}} src={boardGroupPic} id="hero-image" /> */}
       <div class="home-container" >
-        <section class="hero-container" style={{
-          backgroundImage: { illustration }
-        }}>
-          <div>
-            <h1
-              style={{ marginBottom: "2%", fontWeight: "bold" }}
-              class="headerName"
-            >
-              Women in Computing at Cornell
-            </h1>
-            <h2>strives to make computing inclusive for all</h2>
+        <section style = {{ paddingLeft: '5%', width: '100%', height: '100%', backgroundColor:'#f8f9fa',  display: 'flex', flexDirection: 'row', alignContent: 'center', justifyContent: 'space-between'}}>
+          <div style = {{backgroundColor:'#f8f9fa', display : 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+            <h2 style={{ textAlign: 'left', fontWeight: 'bold', maxWidth: '430px'}}>  
+            Our Mission is 
+            to build a supportive tech community for women and allies.</h2>
+            <p style={{textAlign: 'left', maxWidth: '430px'}}>
+              We provide resources
+             to conquer challenges and create opportunities for technical and 
+             leadership growth. We empower people from diverse backgrounds to 
+             make a positive impact on the future of tech. 
+             We embrace all gender identities.
+            </p>
           </div>
+          <Image style={{ maxHeight: '600px', maxWidth: '880px'}} src={newmissionImg} id="mission-image"></Image>
         </section>
-        <section style = {{justifyContent: 'space-around'}}class="mission-container">
+        <section style = {{display : 'flex', flexDirection: 'row', alignContent: 'center', justifyContent: 'center', justifyContent: 'space-around'}} class="semout-container">
         <div>
-            <Image style={{ maxWidth: '450px' }} src={missionImg} id="mission-image"></Image>
+            <Image style={{ maxWidth: '600px' }} src={semoutlook} id="semester-outlook"></Image>
           </div>
-            <div>
-            <h2 style={{ textAlign: 'left', fontWeight: 'bold' }}>  Our Mission</h2>
-            <p style={{textAlign: 'left', maxWidth: '570px'}}>We aim to foster a <strong>supportive community</strong> of women and
-              allies* equipped with the resources needed to recognize
-              and overcome challenges. By creating <strong>opportunities for
-                technical and leadership growth</strong>, we work to ensure that
-              people of all identities are able to discover and pursue
-              their interests and talents in order to <strong>positively impact
-                the future of tech.</strong></p>
-            <p style={{ textAlign: 'left'}}>* All gender identities are welcomed and supported!</p>
-            <h2 style={{ textAlign: 'left', fontWeight: 'bold' }}>  Semester Outlook</h2>
-            <p style={{textAlign: 'left', maxWidth: '570px'}}>WICC is a place where women 
-            and gender minorities feel supported throughout their academic journey and 
-            careers in technology. We are a coalition of leaders who seek to make an 
-            impact on Cornell, Ithaca and our greater community. Achieving this 
-            impact does not just involve WICC e-board, but also you (g-body members),
-             CIS staff, Cornell, and the greater Ithaca community!</p>
+            <div style = {{display : 'flex', flexDirection: 'column', alignContent: 'center', justifyContent: 'center'}}>
+              <h2 style={{ textAlign: 'left', fontWeight: 'bold' }}>  Semester Outlook</h2>
+              <p style={{textAlign: 'left', maxWidth: '570px'}}>
+                WICC is a place where women 
+                and gender minorities feel supported throughout their academic journey and 
+                careers in technology. We are a coalition of leaders who seek to make an 
+                impact on Cornell, Ithaca and our greater community. Achieving this 
+                impact does not just involve WICC e-board, but also you (g-body members),
+                CIS staff, Cornell, and the greater Ithaca community!
+             </p>
           </div>
         </section>
         {/* Title "What we do" */}
-      <section className="what-we-do-container">
+      <section style = {{ paddingLeft: '5%', paddingRight: '5%'}}className="what-we-do-container">
         <div className="title-with-line">
           {/* Title */}
           <h2 className="title-text">What we do</h2>
@@ -102,30 +100,30 @@ const Home = () => {
           <div className="horizontal-line"></div>
         </div>
       </section>
-        <section style={{paddingTop: '5px'}} class="carousel-container">
+        <section style={{paddingLeft: '5%',paddingRight: '5%', paddingTop: '20px'}} class="carousel-container">
           <PCarousel imgNames={homepics} pics={homepicsname} />
       </section>
-      <section style = {{paddingTop: '50px'}} className="alumni-testimonies-container">
+      <section style = {{paddingLeft: '5%', paddingRight: '5%',paddingTop: '50px'}} className="alumni-testimonies-container">
         <div className="alumni-testimonies-content">
           <h2>Alumni Testimonies</h2>
           <div className="testimony-grid">
             <div className="testimony-column">
               {/* Testimony 1 */}
-              <h2 style={{ fontWeight: 'bold', color: 'white', fontSize: 25 }}>Nandita Mohan '20</h2>
-              <p>
+              <h2 style={{padding: '10px', fontWeight: 'bold', color: 'black', fontSize: 22 }}>Nandita Mohan '20</h2>
+              <p style = {{padding: '15px', fontSize: 13}}>
 "If there's one person who first made me feel involved, integrated and comfortable in the CIS community at Cornell, it would be Harini. Being her friend and mentee from when I was a freshman showed me that incredibly successful and humble upperclassmen also had their own confusions and struggles along the way, and that it was in-fact completely normal. Not to mention she was always willing to grab a meal, have fun conversations, and be there for me when I needed it.".</p>
             </div>
             <div className="testimony-column">
               {/* Testimony 2 */}
-              <h2 style={{ fontWeight: 'bold', color: 'white', fontSize: 25 }}>Maya Frai '20</h2>
-              <p>
+              <h2 style={{ padding: '10px', fontWeight: 'bold', color: 'black', fontSize: 22 }}>Maya Frai '20</h2>
+              <p style = {{padding: '15px', fontSize: 13}}>
 “WICC has given me an invaluable amount of resources, one of them being the alumni network. When I first heard about WICC my freshman year, I joined Lunch Bunch and was able to meet two incredible women who I am happy to call my mentors. They took the time to talk about all of the diverse opportunities in tech and how WICC has allowed them to connect with other women in the field. WICC alumni are not only incredibly inspirational, but are also dedicated and down-to-earth. Looking forward to being a part of this admirable network after graduation.”
 </p>
             </div>
             <div className="testimony-column">
               {/* Testimony 3 */}
-              <h2 style={{ fontWeight: 'bold', color: 'white', fontSize: 25 }}>Karen Zhang '21</h2>
-              <p>
+              <h2 style={{ padding: '10px', fontWeight: 'bold', color: 'black', fontSize: 22 }}>Karen Zhang '21</h2>
+              <p style = {{padding: '15px', fontSize: 13}}>
 “I learned about the different fields of CS and how they're used, which I previously had many misconceptions about. Lunch Bunch truly let me cement my decision to major in CS, as through it I got an idea of the potential of the field.”</p>
             </div>
             {/* Add more testimonies as needed */}
