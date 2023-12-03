941819737a417de894e263fc1d292e8e09b336af
Kelly Tran <kelly.tran3252@gmail.com>
2023-12-03 16:20:41 -0500

    Implemented pop-up bubble for member cards

--- src/components/memberCard.js
+++ src/components/memberCard.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "./member.css";
-// import defaultImage from "../images/noimage.png";
+import defaultImage from "../images/noimage.png";
+
+// Ideally, this should be in your main app component or index file
+Modal.setAppElement('#root'); // Replace '#root' with your actual app root element id
 
 function Member({ name, title, netid, bio, img }) {
   const [showDetails, setShowDetails] = useState(false);
-  // const [imageSrc, setImageSrc] = useState(img);
-
-  // const handleError = () => {
-  //   setImageSrc(defaultImage);
-  // };
 
   const toggleDetails = () => setShowDetails(!showDetails);
 
@@ -20,7 +20,7 @@ function Member({ name, title, netid, bio, img }) {
         <div>
           <LazyLoadImage
             alt={name}
-            src={img}
+            src={img || defaultImage} // Fallback to defaultImage if img is not provided
             effect="blur"
             className="imageStyle"
             // onError={handleError}
@@ -28,13 +28,38 @@ function Member({ name, title, netid, bio, img }) {
         </div>
         <div className="title">{title}</div>
         <div className="name">{name}</div>
-        {showDetails && (
-          <div className="bio">
-            <p>{bio}</p>
-            {/* More detailed info can be added here */}
-          </div>
-        )}
       </div>
+
+      <Modal 
+        isOpen={showDetails}
+        onRequestClose={toggleDetails} // Close modal when overlay is clicked
+        contentLabel="Member Details"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.6)' // Semi-transparent overlay
+          },
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            width: '80%',
+            maxWidth: '800px',
+            borderRadius: '15px',
+          },
+        }}
+      >
+        <div className="modalContent">
+          <img src={img || defaultImage} alt={name} className="imageStyle" />
+          <div className="modalTextContent">
+            <h3>{name}</h3>
+            <p><b>Netid:</b> {netid}</p>
+            <p>{bio}</p>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 }
