503e815de1fd7ccdf5d2860fdc3c89e5c391c0e4
HoangMyUy <119373985+HoangMyUy@users.noreply.github.com>
2023-12-07 20:35:30 -0500

    image update

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 import "./pages.css";
-// import BoardCard from "../components/BoardCard.js";
-// import illustration from "../images/homepage-illustration.png";
-// import Navbar from 'react-bootstrap/Navbar';
-// import { Nav, NavItem, NavDropdown } from "react-bootstrap";
 import { sponsorData } from "./sponsorData";
 import Image from "react-bootstrap/Image";
 // import { WICCSponsors } from "./WICCsponsors.pdf";
@@ -55,8 +51,8 @@ export default class Sponsors extends Component {
     );
 
     return (
-      <div className='containerPage'>
-        <div style={{ marginBottom: '10%' }}>
+      <div className="containerPage">
+        <div style={{ marginBottom: "10%" }}>
           {/* <h4 style={{ marginBottom: '3%', fontWeight: 'bold' }}>Gold</h4>
           {gold.map((value) => {
             return <a href={value.link}><Image src={(sponsorHeadshots[value.sponsor + '.jpg'])} width='25%' height='10%' style={{ marginTop: '-6%', paddingRight: '20px' }} /></a>
