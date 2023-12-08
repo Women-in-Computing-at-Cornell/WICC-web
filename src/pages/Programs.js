d093403ed0a0014ecfc3bb9745c1b9ec0f0f8b0a
HoangMyUy <119373985+HoangMyUy@users.noreply.github.com>
2023-12-07 23:14:36 -0500

    Revert "Program page update"
    This reverts commit d42534ed0c5354bdaf49ca497e8f71b521908191.

--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -4,7 +4,6 @@ import Image from "react-bootstrap/Image";
 import { Link } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "./Program.css";
 import "./pages.css";
 import activemember from "../images/programs/activemember.jpg";
 import campaigns from "../images/programs/campaigns.jpg";
@@ -13,7 +12,7 @@ import cyc from "../images/programs/cyc.jpg";
 import lunchbunch from "../images/programs/lunchbunch.jpg";
 import mentorship from "../images/programs/mentorship.jpg";
 import outreach from "../images/programs/outreach.jpg";
-import Programhero from "../images/programs/Programhero.png";
+import { HoverStyle } from "devextreme-react/chart.js";
 
 let styles = {
   container: {
@@ -29,46 +28,8 @@ let styles = {
 
 const Programs = () => {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <div className="hero">
-        <div
-          style={{
-            width: "30%",
-            paddingLeft: "56px",
-            position: "absolute",
-            marginLeft: "15%",
-            marginTop: "10%",
-          }}
-        >
-          <h1
-            style={{
-              fontWeight: "bold",
-              paddingTop: "56px",
-              paddingBottom: "16px",
-            }}
-          >
-            Programs
-          </h1>
-          <p>
-            We are dedicated to fostering an inclusive and empowering community
-            for women and non-binary students at Cornell. Each semester, we
-            curate a dynamic lineup of programs and events designed to not only
-            provide support but also create an environment that is both
-            enriching and informative. Our goal is to continually enhance these
-            offerings, ensuring they resonate with our diverse community and
-            actively engage every participant
-          </p>
-        </div>
-        <Image
-          src={Programhero}
-          style={{ width: "100%", height: "auto" }}
-        ></Image>
-      </div>
+    <div style={{ width: "100%", height: "100%" }}>
+      <h1 style={{ fontWeight: "bold", margin: "200px" }}>Programs</h1>
       <Link to="/membership" style={styles.link}>
         <Row
           style={{
