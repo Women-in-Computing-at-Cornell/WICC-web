5c1e2fabdea5a1025f1186ef20fcf62ef2f69f40
HoangMyUy <119373985+HoangMyUy@users.noreply.github.com>
2023-12-07 22:06:23 -0500

    test

--- src/pages/ResourcesSoon.js
+++ src/pages/ResourcesSoon.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import "./pages.css";
-import BoardCard from "../components/BoardCard.js";
-import Navbar from "react-bootstrap/Navbar";
-import { Nav, NavItem, NavDropdown } from "react-bootstrap";
-import { sponsorData } from "./sponsorData";
-import Image from "react-bootstrap/Image";
+// import React, { Component } from "react";
+// import "./pages.css";
+// import BoardCard from "../components/BoardCard.js";
+// import Navbar from "react-bootstrap/Navbar";
+// import { Nav, NavItem, NavDropdown } from "react-bootstrap";
+// import { sponsorData } from "./sponsorData";
+// import Image from "react-bootstrap/Image";
 
-export default class ResourcesSoon extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "pres",
-      displayData: [],
-    };
-  }
+// export default class ResourcesSoon extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: "pres",
+//       displayData: [],
+//     };
+//   }
 
-  render() {
-    return (
-      <div>
-        <h5>More resources coming soon!</h5>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h5>More resources coming soon!</h5>
+//       </div>
+//     );
+//   }
+// }
