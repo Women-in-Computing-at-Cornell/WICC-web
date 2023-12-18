9eb6adc36ba91e66051c11edf200e1410ba5a288
Alina <ay332@cornell.edu>
2023-12-17 19:09:58 -0800

    Finish bottom half of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,7 +35,7 @@ const PCarousel = ({ imgNames, pics }) => {
             <Image
               src={imageSrc}
               alt={`Image ${i + index + 1}`}
-              style={{ width: "100%", height: "200px", objectFit: "fill" }}
+              style={{ width: "100%", height: "200px", objectFit: "contain" }}
             />
           </div>
         );
@@ -49,6 +49,8 @@ const PCarousel = ({ imgNames, pics }) => {
     ));
   };
 
+
+  
   return (
     <div className="carousel-wrapper">
       <Carousel showThumbs={false} showStatus={false} emulateTouch infiniteLoop>
@@ -57,6 +59,8 @@ const PCarousel = ({ imgNames, pics }) => {
     </div>
   );
 };
+
+
 
 const Home = () => {
   const homepicsname = importAll(
@@ -67,6 +71,15 @@ const Home = () => {
     )
   );
   const homepics = Object.keys(homepicsname);
+
+  const sponsorpicsname = importAll(
+    require.context(
+      "../images/sponsors",
+      false,
+      /\.(jpg)$/i
+    )
+  );
+  const sponsorpics = Object.keys(sponsorpicsname);
 
   return (
     <div>
@@ -158,8 +171,7 @@ const Home = () => {
         {/* Title "What we do" */}
         <section
           style={{ paddingLeft: "5%", paddingRight: "5%" }}
-          className="what-we-do-container"
-        >
+          className="what-we-do-container">
           <div className="title-with-line">
             {/* Title */}
             <h2 className="title-text">What we do</h2>
@@ -251,14 +263,64 @@ const Home = () => {
             </div>
           </div>
         </section>
-        <section style={{ marginTop: "5%" }} class="sponsor-container">
+        {/*<section class="call-to-action-container"> 
+          <h3>Get Involved</h3>
+          <Button href="/#join">Get Involved</Button>
+                </section>  */}
+        <section class ="call-to-action-title">
+        <h2 className="title-text">Get Involved</h2>
+        </section>
+        <section class="call-to-action-container">
+          <div className="action-panel"> 
+            <div className="action-panel-content">
+            <a href="/#join" className="panel-link">
+              <button className="panel-button">
+                <span>&#x2197;</span>
+              </button>
+            </a>
+            <p>Join Us</p>
+           </div>
+        </div>
+        <div className="action-panel">
+          <div className="action-panel-content">
+            <a href="https://drive.google.com/file/d/1s_okJqDYmfD5-3jEPCPKvbtP_TYruXXD/view?usp=sharing" className="panel-link">
+              <button className="panel-button">
+                <span>&#x2197;</span>
+              </button>
+            </a>
+            <p>Sponsors</p>
+          </div>
+        </div>
+        <div className="action-panel">
+          <div className="action-panel-content">
+            <a href="/#programs" className="panel-link">
+              <button className="panel-button">
+                <span>&#x2197;</span>
+              </button>
+            </a>
+            <p>Programs</p>
+          </div>
+        </div>
+      </section>
+
+      <section style={{ marginTop: "5%" }} class="sponsor-container">
+      <h2 style={{textAlign: "left", fontWeight: "bold" }}>Our Sponsors</h2>
+      </section>
+        <section
+          style={{objectFit: "cover", paddingLeft: "5%", paddingRight: "5%", paddingTop: "20px" }}
+          class="sponsor-carousel-container"
+        >
+          <PCarousel imgNames={sponsorpics} pics={sponsorpicsname} />
+        </section>
+
+
+
+      {/*  <section style={{ marginTop: "5%" }} class="sponsor-container">
           <h2 style={{ fontWeight: "bold" }}>Our Sponsors</h2>
           <Sponsors />
         </section>
-        <section class="call-to-action-container">
-          <h3>Believe in this mission?</h3>
-          <Button href="/#join">Get Involved</Button>
-        </section>
+              */}
+        
       </div>
     </div>
   );
