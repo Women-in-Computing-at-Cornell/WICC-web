79c1f3f86faa13bb4b56ea427799e75ba158a597
Rahma Abdullah <ra567@cornell.edu>
2023-12-18 20:15:20 -0500

    Added images and formatted GBody cards

--- src/pages/programs/Discussions.js
+++ src/pages/programs/Discussions.js
@@ -3,15 +3,30 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
 import { boardData } from "../boardData";
 import communityhero from "../../images/programs/communityhero.jpg";
 import ReactGoogleSlides from "react-google-slides";
 import QuestionsCard from "./QuestionsCard.js";
-// import { Gallery } from "react-grid-gallery";
 
-import Image from "react-bootstrap/Image";
+//wicc away imports (note: use a function instead)
+import group1 from "../../images/programs-pictures/wiccaway/group1.JPG";
+import painting1 from "../../images/programs-pictures/wiccaway/painting1.jpg";
+import painting2 from "../../images/programs-pictures/wiccaway/painting2.JPG";
+import group3 from "../../images/programs-pictures/wiccaway/group3.JPG";
+import painting3 from "../../images/programs-pictures/wiccaway/painting3.JPG";
+
+//gbody imports
+import friendsgiving from "../../images/programs-pictures/gbody1.png";
+import pumpkinspice from "../../images/programs-pictures/gbody3.png";
+import advocacy from "../../images/programs-pictures/gbody2.png";
+
+//gbody social imports
+import social1 from "../../images/programs-pictures/socials/dodgeball2.png";
+import social2 from "../../images/programs-pictures/socials/tree1.png";
+import social3 from "../../images/programs-pictures/socials/dodgeball1.png";
 
 import medal from "../../images/medal.jpg";
 
@@ -63,15 +78,15 @@ export default class Discussions extends Component {
 
     const GBodyCardData = [
       {
-        src: "https://images.unsplash.com/photo-1590424744257-fdb03ed78ae0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-        title: "November 2023: Friendsgiving!",
+        src: friendsgiving,
+        title: "Nov 2023: Friendsgiving!",
         caption:
           "In our November GBody, we celebrated the season of giving\
         thanks and came together with our Bowers CiS community for\
         a Friendsgiving event!",
       },
       {
-        src: "https://images.unsplash.com/photo-1590424744257-fdb03ed78ae0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
+        src: pumpkinspice,
         title: "Oct 2023: Pumpkin Spice & Chill~",
         caption:
           "At our second semesterly GBody, we took some time to\
@@ -79,7 +94,7 @@ export default class Discussions extends Component {
         pumpkins and drinking pumpkin spiced Swiss Miss.",
       },
       {
-        src: "https://images.unsplash.com/photo-1590424744257-fdb03ed78ae0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
+        src: advocacy,
         title: "Sept 2023: Self-Advocacy",
         caption:
           "In our first GBody of the fall semester, WICC members dove\
@@ -93,11 +108,9 @@ export default class Discussions extends Component {
       return (
         <div class="item">
           <a aria-labelledby="person"></a>
-          <div class="card-img">
-            <img
-              src={src}
-              alt=""
-            ></img>
+          <div class="card-img-div">
+            <img src={src} alt="GBody Image" class="card-img"></img>
+            <div class="img-overlay"></div>
           </div>
           <div class="item__overlay">
             <h3 id="person" aria-hidden="true">
@@ -115,8 +128,97 @@ export default class Discussions extends Component {
       return (
         <div class="grid">
           {GBodyCardData.map((image, index) => (
-            <GBodyCard key={index} src={image.src} caption={image.caption} title={image.title} />
+            <GBodyCard
+              key={index}
+              src={image.src}
+              caption={image.caption}
+              title={image.title}
+            />
           ))}
+        </div>
+      );
+    };
+
+    const WiccAwayImages = [
+      //programs-pictures : mentorship2
+      group1,
+      painting1,
+      painting2,
+      painting3,
+      group3,
+    ];
+
+    const SocialImages = [
+      social1,
+      social2,
+      social3,
+    ];
+
+    const CarouselComponent = ({ images }) => {
+      const [index, setIndex] = useState(0);
+
+      const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+      };
+
+      return (
+        <div>
+          <div class="image-caption-container">
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              indicators={false}
+              class="carousel"
+            >
+              {images.map((imageUrl, index) => (
+                <Carousel.Item key={index}>
+                  <img
+                    src={imageUrl}
+                    alt={`Slide ${index}`}
+                    className="carousel-image"
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+            {/* </div> */}
+            <div class="caption">
+              <h2>WICC Away!</h2>
+              <p>
+                This was our WICC Away event where kicked off the Fall semester.
+                We introduced WICC and our mission to attendees, hosted an
+                allyship workshop, had a painting session, then wrapped up our
+                night with a WICC Q&A Panel!
+              </p>
+            </div>
+          </div>
+          <br />
+          <div class="image-caption-container">
+            <div class="caption">
+              <h2>G-Body Socials</h2>
+              <p>
+                This was our WICC Away event where kicked off the Fall semester.
+                We introduced WICC and our mission to attendees, hosted an
+                allyship workshop, had a painting session, then wrapped up our
+                night with a WICC Q&A Panel!
+              </p>
+            </div>
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              indicators={false}
+              class="carousel"
+            >
+              {images.map((imageUrl, index) => (
+                <Carousel.Item key={index}>
+                  <img
+                    src={imageUrl}
+                    alt={`Slide ${index}`}
+                    className="carousel-image"
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
         </div>
       );
     };
@@ -128,14 +230,14 @@ export default class Discussions extends Component {
           <div class="title">
             <div class="typewriter">
               <b>
-                <h1>Community Discussions :)</h1>
+                <h1>Community Connections :)</h1>
               </b>
             </div>
           </div>
         </div>
 
-        {/* Blurb */}
         <div class="page">
+          {/* Blurb */}
           <p class="header">
             WICC is dedicated towards fostering an inclusive space for
             individuals of all identities to feel empowered to speak on issues
@@ -148,60 +250,25 @@ export default class Discussions extends Component {
           <br></br>
           <br></br>
 
-          <div class="wicc-away">
-            <h3>G-Body // G-Body Social</h3>
+          <div class="gbody">
+            <h3>G-Body Meetings</h3>
             <br></br>
             {/* Cards */}
-            <GBodyCards/>
+            <GBodyCards />
+          </div>
+
+          <br />
+          <br />
+          <br />
+
+          <h3>Socials!</h3>
+          <div class="left-away">
+            <div class="left-half">
+              <CarouselComponent images={WiccAwayImages} />
+            </div>
           </div>
           <br></br>
           <br></br>
-          <h4>Last Community Discussions: </h4>
-          <br />
-          <h5>September 2023</h5>
-          <h6>
-            This was our WICC Away event where kicked off the Fall semester. We
-            introduced WICC and our mission to attendees, hosted an allyship
-            workshop, had a painting session, then wrapped up our night with a
-            WICC Q&A Panel!
-          </h6>
-          <br />
-          <ReactGoogleSlides
-            width={640}
-            height={480}
-            slidesLink="https://docs.google.com/presentation/d/1qLtKtR55spx-zBkL9bWjo_P2rAm3oCnIGjADSgGq0Z0/edit?usp=sharing"
-            showControls
-          />
-          <br />
-          <h5>Febuary 2022</h5>
-          <h6>
-            This was our G-Body #1 where kicked off the Spring semester. We met
-            WICC faculty advisors and reflected on our Fall semester to get
-            ready for the new semester. We learned about setting and completing
-            goals through intention framing and journaling.
-          </h6>
-          <br />
-          <ReactGoogleSlides
-            width={640}
-            height={480}
-            slidesLink="https://docs.google.com/presentation/d/1cHwUJEC7DM9ekD_KSajxkrtxeZPzD_jqQ4-uymKIZro/edit?usp=sharing"
-            showControls
-          />
-          <br />
-          <h5>March 2022</h5>
-          <h6>
-            This was our G-Body #2 where we had discussions about taking care of
-            ourselves and having empathy for others. At the end, we created a
-            WICC wall of notes and got to know each other.{" "}
-          </h6>
-          <br />
-          <ReactGoogleSlides
-            width={640}
-            height={480}
-            slidesLink="https://docs.google.com/presentation/d/1SNCHd_FxDbFiqw7yx5JubO15NsFeFsFfrTXV79L2Ap8/edit#slide=id.gfd5af8f16f_0_15"
-            showControls
-          />
-          <br />
           <center>
             <h5 style={{ paddingTop: "5%" }}>Questions?</h5>
             <Container style={{ paddingRight: "10%" }}>
