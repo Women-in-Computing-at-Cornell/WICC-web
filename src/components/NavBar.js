aa8ce3206f630ba3289d1f3668cc8b7223d5dab6
Alina <ay332@cornell.edu>
2024-03-17 14:20:49 -0400

    fix nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,33 +3,46 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Watermark from '../images/thick-wicc-logo.png';
 import './nav.css';
+import Button from 'react-bootstrap/Button';
 
 
 class NavBar extends React.Component {
   render() {
+    const linkStyle = {
+      marginLeft: '10px', // Adjust the right margin to space out the links
+    };
     return (
       <Navbar bg="light" expand="lg">
         <Navbar.Brand className="mr-auto" href="/">
           <img
             src={Watermark}
-            width="30"
-            height="35"
+            width="35"
+            height="45"
             alt="WICC logo"
           />
+          <div className="brand-text">
+            <p>Women in Computing <br/> at Cornell</p>
+          </div>
         </Navbar.Brand>
         <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/#about">About</Nav.Link>
-          <Nav.Link href="/#board">Who We Are</Nav.Link>
-          <Nav.Link href="/#programs">Programs</Nav.Link>
+          <Nav.Link style={linkStyle} href="/">Home</Nav.Link>
+          <Nav.Link style={linkStyle} href="/#about">About</Nav.Link>      
+          <Nav.Link style={linkStyle} href="/#programs">Programs</Nav.Link>
+          <Nav.Link style={linkStyle} href="/#calendar">Calendar</Nav.Link> 
+          <Nav.Link style={linkStyle} href="/#sponsors">Sponsors</Nav.Link>
 
-          <Nav.Link href="/#calendar">Calendar</Nav.Link>
-          <Nav.Link href="/#join">Get Involved</Nav.Link>
+          <div className="join-us-button">
+            <Button style={linkStyle} href="/#join">Join Us</Button>
+          </div>
         </Nav>
       </Navbar>
     );
   }
 }
 //<Nav.Link href="/#resources">Resources</Nav.Link>//
+//<Nav.Link href="/#board">Who We Are</Nav.Link>//
+//<Nav.Link href="/#join">Join Us</Nav.Link>
+
 
 export default NavBar;
+
