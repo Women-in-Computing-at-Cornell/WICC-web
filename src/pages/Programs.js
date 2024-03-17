92c11118d4dc7dce6cf23a6e996fad15c4654700
Ruoxin Chen <rc753@cornell.edu>
2024-03-17 15:13:08 -0400

    Fixed progress bar

--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -16,6 +16,8 @@ import outreach from "../images/programs/outreach.jpg";
 import Programhero from "../images/programs/Programhero.png";
 import EventCard from "../components/eventCard";
 import Card from "../components/Card";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import Progressbar from "../components/ProgressBar";
 
 let styles = {
   container: {
@@ -29,6 +31,17 @@ let styles = {
   },
 };
 
+let events = [
+  <EventCard key="1" title="Upcoming Event 1" description="xxx" imageUrl={activemember} />,
+  <EventCard key="2" title="Upcoming Event 2" description="yyy" imageUrl={activemember} />,
+  <EventCard key="3" title="Upcoming Event 3" description="zzz" imageUrl={activemember} />,
+  <EventCard key="4" title="Upcoming Event 4" description="xxx" imageUrl={activemember} />,
+  <EventCard key="5" title="Upcoming Event 5" description="yyy" imageUrl={activemember} />,
+  <EventCard key="6" title="Upcoming Event 6" description="zzz" imageUrl={activemember} />,
+  <EventCard key="7" title="Upcoming Event 7" description="yyy" imageUrl={activemember} />,
+  <EventCard key="8" title="Upcoming Event 8" description="zzz" imageUrl={activemember} />
+];
+
 const Programs = () => {
   const [startIdx, setStartIdx] = useState(0)
   const [endIdx, setEndIdx] = useState(3)
@@ -37,7 +50,7 @@ const Programs = () => {
 
   useEffect(() => {
     setCurrEvents(events.slice(startIdx, endIdx));
-    setProgress(endIdx / events.length);
+    setProgress((endIdx / events.length) * 100);
   }, [startIdx, endIdx, events]);
 
   function updateEventsRight() {
@@ -103,7 +116,36 @@ const Programs = () => {
           style={{ width: "100%", height: "auto" }}
         ></Image>
       </div>
-      <EventCard title="Active Membership" description="Active Membership " imageUrl={activemember} />
+      <div className="button-display">
+        <text className="upcoming-title">
+          Upcoming Events 2023-2024
+        </text>
+        <div>
+          {startIdx > 0 && <button
+            onClick={updateEventsLeft}
+            style={{ border: 'none', background: 'transparent', cursor: 'pointer', marginRight: '40px' }}>
+            <FaChevronLeft size={24} />
+          </button>}
+          {endIdx < events.length && <button
+            onClick={updateEventsRight}
+            style={{ border: 'none', background: 'transparent', cursor: 'pointer' }}>
+            <FaChevronRight size={24} />
+          </button>}
+        </div>
+      </div>
+      <Progressbar
+        bgcolor="#9CE2D3"
+        progress={progress}
+        height={4}
+      />
+      <div className="event-container">
+        {currEvents.map((event, index) => (
+          <React.Fragment key={index}>
+            {event}
+          </React.Fragment>
+        ))
+        }
+      </div>
       <Link to="/membership" style={styles.link}>
         <Row
           style={{
