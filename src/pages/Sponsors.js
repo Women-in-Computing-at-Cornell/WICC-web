68353bd9f9aeff346dbc22e7423f4be6862e833b
Amy Sharin <amysharin1@gmail.com>
2024-03-26 17:39:16 -0400

    minor font changes

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -32,61 +32,70 @@ const Sponsors = () => {
         ></Image>
       </section>
       <div>
-        <section 
-          style = {{
+        <section
+          style={{
             display: "flex",
-            width: "1728px",
+            width: "100%",
             padding: "96px 150px",
             alignItems: "center",
             gap: "160px",
             background: "var(--3, #FFF)",
-        }}>
+          }}
+        >
           <Image
-            style={{ maxHeight: "600px", maxWidth: "400px", width: "100%", borderRadius: "30px"
-             }}
+            style={{
+              maxHeight: "600px",
+              maxWidth: "400px",
+              width: "100%",
+              borderRadius: "30px",
+            }}
             src={sponsorsCandid}
           ></Image>
           <div
             style={{
-                display: "flex",
-                flexDirection: "column",
-            }}>
-              <h2
-                style = {{
-              color: "#000",
-              fontFamily: "Mulish",
-              fontSize: "36px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%",
-            }}>
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <h2
+              style={{
+                color: "#000",
+                fontFamily: "Mulish",
+                fontSize: "36px",
+                fontStyle: "normal",
+                fontWeight: "700",
+                lineHeight: "150%",
+              }}
+            >
               Benefits
             </h2>
             <p
-            style = {{
-              alignSelf: "stretch",
-              color: "#000",
-              fontFamily: "Mulish",
-              fontSize: "20px",
-              fontStyle: "normal",
-              fontWeight: "400",
-              lineHeight: "150%",
-              maxWidth: "650px",
-              maxHeight: "240px"
-            }}>
-              Every day, we are working to foster a sense of allyship among intersectional
-              identities in the Cornell community, while also empowering the future of 
-              computing through Girls Who Code outreach program. We believe that these initiatives
-              will translate to healthier and more diverse tech spheres in the future when the
-              growing number of individuals touched by our work enter the workforce. 
-              Your generosity and funding, makes us who we are as an organization, 
-              and in return, we hope to connect you to our network of driven, passionate 
-              women in computing at Cornell.
+              style={{
+                alignSelf: "stretch",
+                color: "#000",
+                fontFamily: "Mulish",
+                fontSize: "19px",
+                fontStyle: "normal",
+                fontWeight: "400",
+                lineHeight: "150%",
+                maxWidth: "650px",
+                maxHeight: "240px",
+              }}
+            >
+              Every day, we are working to foster a sense of allyship among
+              intersectional identities in the Cornell community, while also
+              empowering the future of computing through Girls Who Code outreach
+              program. We believe that these initiatives will translate to
+              healthier and more diverse tech spheres in the future when the
+              growing number of individuals touched by our work enter the
+              workforce. Your generosity and funding, makes us who we are as an
+              organization, and in return, we hope to connect you to our network
+              of driven, passionate women in computing at Cornell.
             </p>
           </div>
         </section>
       </div>
-      
+
       <CurrentSponsors />
     </div>
   );
