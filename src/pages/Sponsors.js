bc86979493d16534e88b46688e02bdfa06f0b012
ArielleNu <ariellenudelman2@gmail.com>
2024-11-03 15:31:05 -0500

    changed the three containers

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -80,133 +80,38 @@ const Sponsors = () => {
       </div>
 
       <section className="container-section">
-        <div style={{
-            background: "#FFF",
-            borderRadius: "10px",
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
-            padding: "20px",
-            flex: "0 1 calc(25% - 20px)", // 75% width of the middle box
-            maxWidth: "calc(25% - 10px)", // Adjusted width, reduced margin
-            marginLeft: "40px",
-          }}
-        >
-          <h2
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "30px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%", // 45px
-              textAlign: "left",
-            }}
-          >
+        <div className="container1">
+          <h2 className="container-header">
             Listserv Fee
           </h2>
-          <p
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "16px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%", // 24px
-              textAlign: "left",
-              marginBottom: 0, // Removed margin-bottom
-            }}
-          >
+          <p className="container-subheader">
             Want to publicize your company event to 2000+ WICC members?
           </p>
           <br />
-          <div
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "13px",
-              fontStyle: "normal",
-              fontWeight: "400",
-              lineHeight: "150%",
-            }}
-          >
+          <div className="description">
             $50 to publize company events on WICC Listserv. Note: A company
             event entails an event that is not in collaboration with WICC. This
             fee is waived for all Sliver, Gold, and Platinum WICC Sponsors.
           </div>
         </div>
 
-        <div
-          style={{
-            background: "#FFF",
-            borderRadius: "10px",
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
-            padding: "20px",
-            flex: "1 1 calc(40% - 20px)", // Adjusted width, reduced margin
-            maxWidth: "calc(40% - 10px)", // Adjusted width, reduced margin
-            marginRight: "5px", // Reduced margin between the boxes by half
-          }}
-        >
-          <h2
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "30px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%", // 45px
-              textAlign: "left",
-            }}
-          >
+        <div className="container2">
+          <h2 className="container-header">
             Other Ways to Support Us
           </h2>
           <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li
-              style={{
-                color: "var(--2, #212529)",
-                fontFamily: "Mulish",
-                fontSize: "16px",
-                fontStyle: "normal",
-                fontWeight: "700",
-                lineHeight: "150%",
-              }}
-            >
+            <li className="container-subheader">
               -$1200/student Grace Hopper Scholarship
             </li>
-            <li
-              style={{
-                color: "var(--2, #212529)",
-                fontFamily: "Mulish",
-                fontSize: "16px",
-                fontStyle: "normal",
-                fontWeight: "700",
-                lineHeight: "150%",
-              }}
-            >
+            <li className="container-subheader">
               -$1200/student Tapia Conference
             </li>
-            <li
-              style={{
-                color: "var(--2, #212529)",
-                fontFamily: "Mulish",
-                fontSize: "16px",
-                fontStyle: "normal",
-                fontWeight: "700",
-                lineHeight: "150%",
-              }}
-            >
+            <li className="container-subheader">
               -Sponsor a mentorship event, social event, or general body
               meeting.
             </li>
           </ul>
-          <div
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "13px",
-              fontStyle: "normal",
-              fontWeight: "400",
-              lineHeight: "150%",
-            }}
-          >
+          <div className="description">
             Note: Hosting an event without sponsorship is possible. There is a
             single Event Fee (In-Person and Virtual): $400 for both
             public/invite-only events, free for non-profits. We reserve the
@@ -215,71 +120,21 @@ const Sponsors = () => {
           </div>
         </div>
 
-        <div
-          style={{
-            background: "#FFF",
-            borderRadius: "10px",
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
-            padding: "20px",
-            flex: "0 1 calc(25% - 20px)", // 75% width of the middle box
-            maxWidth: "calc(25% - 10px)", // Adjusted width, reduced margin
-            marginRight: "40px",
-          }}
-        >
-          <h2
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "30px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%", // 45px
-              textAlign: "left",
-            }}
-          >
+        <div className="container3">
+          <h2 className="container-header">
             Tiers
           </h2>
-          <p
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "16px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%", // 24px
-              textAlign: "left",
-              marginBottom: 0, // Removed margin-bottom
-            }}
-          >
+          <p className="container-subheader">
             Silver ($1000)
           </p>
-          <p
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "16px",
-              fontStyle: "normal",
-              fontWeight: "700",
-              lineHeight: "150%", // 24px
-              textAlign: "left",
-              marginBottom: 0, // Removed margin-bottom
-            }}
-          >
+          <p className="container-subheader">
             Gold ($2000)
-            <br />
+          </p>
+          <p className="container-subheader">
             Platinum ($3000)
           </p>
           <br />
-          <div
-            style={{
-              color: "var(--2, #212529)",
-              fontFamily: "Mulish",
-              fontSize: "13px",
-              fontStyle: "normal",
-              fontWeight: "400",
-              lineHeight: "150%",
-            }}
-          >
+          <div className="description">
             We have varying levels of sponsorship with different benefits below.
             Your support makes a big difference!
           </div>
